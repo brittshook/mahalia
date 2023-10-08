530cf56271ed0c5c0056d4363245be60cfa171e8
brittshook <122318069+brittshook@users.noreply.github.com>
2023-10-07 20:44:59 -0400

    format validiation

--- modules/inputValidation.js
+++ modules/inputValidation.js
@@ -14,13 +14,13 @@ const inputRules = {
     'email': {
         required: true,
         max: 50,
-        format: 'insert regex'
+        format: 'email'
     },
     'phone': {
         required: true,
         min: 4,
         max: 20,
-        format: 'insert regex'
+        format: 'phone'
     },
     'placement': {
         required: true,
@@ -30,13 +30,11 @@ const inputRules = {
         required: true,
         min: 0.5,
         max: 50,
-        format: 'insert regex'
     },
     'height': {
         required: true,
         min: 0.5,
         max: 50,
-        format: 'insert regex'
     },
     'pronoun': {
         required: true,
@@ -65,16 +63,14 @@ let errorMessage = '';
 function inputValidation(input) {
     const type = input.getAttribute('type');
     const id = input.getAttribute('id');
-    console.log(`type is ${type}, id is ${id}, name is ${input.getAttribute('name')}, checked is ${input.checked}`);
-    console.log(inputRules[input.getAttribute('name')]);
+    const value = input.value;
 
     const hasProp = (prop, name = input.getAttribute('name')) => inputRules[name].hasOwnProperty(prop);
     const getProp = (prop, name = input.getAttribute('name')) => inputRules[name][prop];
     const setProp = (prop, value, name = input.getAttribute('name')) => inputRules[name][prop] = value;
 
-
     if (type === 'text' || type ===  'email' || type === 'tel' || id === 'idea') {
-        const length = input.value.length;
+        const length = value.length;
 
         if (getProp('required') && !length) {
             errorMessage = `Please enter your`; // make a nice string based on name
@@ -83,8 +79,16 @@ function inputValidation(input) {
         } else if (hasProp('min') && length < getProp('min')) {
             errorMessage = `Please enter at least ${getProp('min')} characters.`;
         } else if (hasProp('format')) {
-            return true; // testing purposes
-            // calculate regex
+            let regex;
+            if (getProp('format') === 'email') {
+                regex = /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/i;
+                errorMessage = 'Please enter a valid email address.';
+            } else if (getProp('format') === 'phone') {
+                regex = /^(?:[+\d()\s-]*\d[+\d()\s-]*)*(?:(?:\s?(?:ext|x)\.?\s?)?\d+)?$/; // TODO: test international numbers
+                errorMessage = 'Please enter a valid phone number.';
+            } 
+            
+            return regex.test(value);
         } else {
             if (!getProp('required')) { 
                 setProp('unset', true);
@@ -92,15 +96,10 @@ function inputValidation(input) {
             return true;
         }
     } else if (type ===  'number') {
-        const value = input.value;
-
         if (hasProp('max') && value > getProp('max')) {
             errorMessage = `Please enter less than ${getProp('max')} inches.`;
         } else if (hasProp('min') && value < getProp('min')) {
             errorMessage = `Please enter at  least ${getProp('min')} inches.`;
-        } else if (hasProp('format')) { 
-            return true; // testing purposes
-            // calculate regex
         } else {
             return true;
         }
@@ -118,11 +117,16 @@ function inputValidation(input) {
                 setProp('required', false, 'custom-pronoun');
                 setProp('unset', true, 'custom-pronoun');
                 customTextField.removeAttribute('required');
+                customTextField.setAttribute('placeholder', 'Write My Own')
+                customTextField.setAttribute('disabled', 'true');
+
+                console.log(customTextField.getAttribute('value'));
                 toggleInputUI(customTextField);
             } else if (isChecked) {
                 setProp('required', true, 'custom-pronoun');
                 setProp('unset', false, 'custom-pronoun');
                 customTextField.setAttribute('required', 'true')
+                customTextField.removeAttribute('disabled');
             }
         }
 
