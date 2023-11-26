5040708803e8051a0077ea409fd5e5ab4f3a0741
Britt Shook <brittanysshook@gmail.com>
2023-11-26 17:23:55 -0500

    add logic to toggle mobile menu modal

---
+++ modules/menu.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const openMenuBtn = document.querySelector('.open-menu');
+    const closeMenuBtns = document.querySelectorAll('.close-menu');
+
+    openMenuBtn.addEventListener('click', () => {
+        document.querySelector('#menu-window').style.display = 'flex';
+        document.body.style.overflow = 'hidden';
+    });
+
+    closeMenuBtns.forEach(btn => {
+        btn.addEventListener('click', () => {
+            document.querySelector('#menu-window').style.display = 'none';
+            document.body.style.overflow = 'visible';
+        })
+    })
+});
+
